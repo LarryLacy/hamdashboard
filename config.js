8b207a1e4240afb16796a74726e68f4ce7eb1796
Larry D. Lacy <llacy@SysWiz.net>
2024-08-22 10:34:48 -0600

    Adding propegation

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ const aIMG = [
 //  ["GREY LINE", "https://www.dxfuncluster.com/grayline/"],
 //  ["GREY LINE", "https://www.timeanddate.com/scripts/sunmap.php?iso=now"],
   [
-    "SATELLITE CANADA AND US",
+    "SATELLITE",
     "https://cdn.star.nesdis.noaa.gov/GOES16/GLM/CONUS/EXTENT3/GOES16-CONUS-EXTENT3-625x375.gif",
     //"https://cdn.star.nesdis.noaa.gov/GOES16/GLM/SECTOR/can/EXTENT3/GOES16-CAN-EXTENT3-1125x560.gif",
   ],
@@ -95,15 +95,29 @@ const aIMG = [
     "https://www.blitzortung.org/en/Images/image_b_mn.png",
   ],
   [
-    "HOURLY FORCAST",
+    "FORCAST",
     "https://forecast.weather.gov/meteograms/Plotter.php?lat=41.8774&lon=-103.7542&wfo=CYS&zcode=NEZ019&gset=25&gdiff=7&unit=0&tinfo=MY7&ahour=0&pcmd=11011111111110000000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=48",
+    "https://www.wpc.ncep.noaa.gov/noaa/noaa.gif",
+    "https://www.wpc.ncep.noaa.gov/noaa/noaad2.gif",
+    "https://www.wpc.ncep.noaa.gov/noaa/noaad3.gif",
+    "https://www.wpc.ncep.noaa.gov/qpf/fill_94qwbg.gif",
+    "https://www.wpc.ncep.noaa.gov/qpf/fill_94qwbg.gif",
 //    "SW BROADCAST",
 //    "http://www.short-wave.info/php/transmitter-site-map.php?mobile=false&lat=24.01|36.21|27.46|35.62|-17.53|46.34|50.73|27.46|36.21|39.75|52.65|50.48|50.89|52.65|43.51|52.67|46.34|59.75|36.21|29.84|27.46|27.46|46.34|27.46|27.46|39.36|33.50|39.36|24.88|39.75|-38.83|24.88|24.88|59.75|21.14|36.28|13.28|44.15|24.88|18.22|24.88|21.14|36.21|40.41|21.93|13.28|13.23|34.38|44.15|39.36|39.91|36.21|36.21|39.36|39.40|21.93&lon=90.26|-86.89|-80.93|-86.01|146.05|-67.83|4.39|-80.93|-86.89|116.81|12.91|6.52|-113.85|12.91|-79.63|9.75|-67.83|-151.74|-86.89|-90.12|-80.93|-80.93|-67.83|-80.93|-80.93|75.72|-86.47|75.72|102.49|116.81|176.42|102.49|102.50|-151.74|105.42|-86.10|144.67|86.90|102.50|-63.02|102.49|105.42|-86.89|45.19|59.63|144.67|77.22|108.61|86.90|75.72|-76.58|-86.89|-86.89|75.72|32.86|59.63&freq=4750|4840|5010|5050|5055|5130|5780|5850|5935|5955|5990|6005|6030|6045|6070|6160|6160|7355|7490|7505|7570|7730|9330|9395|9455|9460|9470|9500|9600|9645|9700|9730|9760|9795|9840|9940|11590|11650|11760|11775|11980|12020|13845|15205|15295|15400|15410|15555|15590|15660|15720|15825|15825|17490|17620|17790&az=320|40|181|45|ND|245|ND|315|85|95|233|10|ND|240|ND|ND|245|270|85|20|315|44|245|355|285|174|335|209|163|215|325|225|135|270|177|40|285|212|135|320|177|177|90|305|10|290|60|292|308|173|62|46|46|308|310|80",
   ],
   ["HF PROPAGATION",
   "https://www.hamqsl.com/solar101vhf.php",
   "https://www.hamqsl.com/solar100sc.php",
-  "https://www.hamqsl.com/solarpich.php"],
+  "https://www.hamqsl.com/solarpich.php"
+  "https://img.propagation.dr2w.de/n-america/10M/dr2w_animation_10M.gif,
+  "https://img.propagation.dr2w.de/n-america/12M/dr2w_animation_12M.gif,
+  "https://img.propagation.dr2w.de/n-america/15M/dr2w_animation_15M.gif,
+  "https://img.propagation.dr2w.de/n-america/17M/dr2w_animation_17M.gif,
+  "https://img.propagation.dr2w.de/n-america/20M/dr2w_animation_20M.gif,
+  "https://img.propagation.dr2w.de/n-america/30M/dr2w_animation_30M.gif,
+  "https://img.propagation.dr2w.de/n-america/40M/dr2w_animation_40M.gif,
+  "https://img.propagation.dr2w.de/n-america/60M/dr2w_animation_60M.gif,
+  "https://img.propagation.dr2w.de/n-america/80M/dr2w_animation_80M.gif,
 ];
 
 // Image rotation intervals in milliseconds per tile - If the line below is commented, all tiles will be rotated every 30000 milliseconds (30s)
